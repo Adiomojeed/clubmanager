24807564d66ad9e2a37dd94aad212581c063c83d
Adiomojeed <adio.mojeed@gmail.com>
2020-05-04 11:55:36 +0100

    Completed project setup

---
+++ src/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/styles/app.scss'
+
+
+const App = () => {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
